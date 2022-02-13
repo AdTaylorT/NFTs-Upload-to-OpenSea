24bdd5ecc9ec85b16d2164197713e42709bff544
Adam <s7ae1k3r@gmail.com>
2022-02-13 03:33:41 -0500

    Added static object and methods SaleItem

--- openseaupload.py
+++ openseaupload.py
@@ -21,7 +21,7 @@ gui.geometry('450x350')
 gui.title("NFTs Upload to OpenSea  ")
 main_directory = os.path.join(sys.path[0])
 is_polygon = BooleanVar(value=False)
-path_to_nfts = ""
+path_to_nfts = None
 
 
 class IField:
@@ -48,6 +48,34 @@ class IField:
         self.input_field.insert(0, text)
 
 
+class SaleItem:
+    """ Static object placeholder for fields """
+    current_num = 0  # counter
+
+    collection_title = ""
+    collection_ext_link = ""
+    item_price = 1
+    item_image_format = "png"
+    item_file_path = ""
+    item_description = ""
+
+    @staticmethod
+    def increment_current_item():
+        SaleItem.current_num += 1
+
+    @staticmethod
+    def get_current_item_str():
+        return str(SaleItem.current_num)
+
+    @staticmethod
+    def get_current_item_absolute_path():
+        file_name_w_ext = '.'.join([SaleItem.get_current_item_str(), SaleItem.item_image_format])
+        file_path = os.path.join(SaleItem.item_file_path, file_name_w_ext)
+        if os.path.isfile(file_path):
+            return os.path.abspath(file_path)
+        raise FileNotFoundError
+
+
 """
 Initialize InputFields in gui
 the inputs from the form according to their input fields
@@ -56,117 +84,119 @@ collection_link_input = IField("OpenSea Collection Link:", 2, 0, gui)
 start_num_input = IField("Start Number:", 3, 0, gui)
 end_num_input = IField("End Number:", 4, 0, gui)
 price = IField("Price:", 5, 0, gui)
-title = IField("Title:", 6, 0, gui)
+title = IField("Collection Title:", 6, 0, gui)
 description = IField("Description:", 7, 0, gui)
 file_format = IField("NFT Image Format:", 8, 0, gui)
 external_link = IField("External link:", 9, 0, gui)
 folder_location = IField("", 21, 0, gui, 80)
-wait = WebDriverWait()
 
 
 def main():
     """Start The Application """
     print("Start Application ... ")
     project_path = main_directory
-    file_path = folder_location.input_field.get()
-    collection_link = collection_link_input.input_field.get()
-    start_num = int(start_num_input.input_field.get())
-    current_item_num = start_num
     end_num = int(end_num_input.input_field.get())
 
-    item_price = float(price.input_field.get())
-    item_title = title.input_field.get() + str(start_num)
-    item_image_format = file_format.input_field.get()
-    item_ext_link = str(external_link.input_field.get())
-    item_description = description.input_field.get()
+    SaleItem.collection_link = collection_link_input.input_field.get()
+    SaleItem.collection_title = title.input_field.get()
+    SaleItem.collection_ext_link = str(external_link.input_field.get())
+    SaleItem.item_price = float(price.input_field.get())
+    SaleItem.item_file_path = folder_location.input_field.get()
+    SaleItem.item_image_format = file_format.input_field.get()
+    SaleItem.item_description = description.input_field.get()
+    SaleItem.current_num = (int(start_num_input.input_field.get()))
 
     web_driver = init_chrome_options(project_path)
-    global wait
     wait = WebDriverWait(web_driver, 15)
 
-    while current_item_num <= end_num:
-        print("Start creating NFT " + item_title)
+    web_driver.get(SaleItem.collection_ext_link)
+    print(f"Start creating NFTs in Collection: [{SaleItem.collection_title:s}]")
+    while SaleItem.current_num <= end_num:
+        upload_all_files(SaleItem, wait, web_driver)
 
-        web_driver.get(collection_link)
-        wait_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/span/a')
-        add_item_link = web_driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/span/a')
-        add_item_link.click()
-        time.sleep(1)
+    print("Done with all Items")
 
-        wait_xpath('//*[@id="media"]')
-        image_upload_link = web_driver.find_element_by_xpath('//*[@id="media"]')
-        file_path = os.path.join(file_path, '.'.join([str(current_item_num), item_image_format]))
-        image_path = os.path.abspath(file_path)
-        image_upload_link.send_keys(image_path)
 
-        name = web_driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(item_title)  # +1000 for other folders #change name before "#"
-        time.sleep(0.5)
+def upload_all_files(item, wait, web_driver):
+    wait_xpath(wait, '//*[@id="__next"]/div[1]/main/div/div/div[1]/span/a')
+    add_item_link = web_driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/span/a')
+    add_item_link.click()
+    time.sleep(1)
 
-        ext_link = web_driver.find_element_by_xpath('//*[@id="external_link"]')
-        ext_link.send_keys(item_ext_link)
-        time.sleep(0.5)
+    wait_xpath(wait, '//*[@id="media"]')
+    image_upload_link = web_driver.find_element_by_xpath('//*[@id="media"]')
+    image_upload_link.send_keys(item.get_current_item_absolute_path())
 
-        desc = web_driver.find_element_by_xpath('//*[@id="description"]')
-        desc.send_keys(item_description)
-        time.sleep(0.5)
+    name = web_driver.find_element_by_xpath('//*[@id="name"]')
+    name.send_keys(item.collection_title)  # +1000 for other folders #change name before "#"
+    time.sleep(0.5)
 
-        # Select Polygon blockchain if applicable
-        if is_polygon.get():
-            blockchain_button = \
-                web_driver.find_element(By.XPATH,
-                                        '//*[@id="__next"]/div[1]/main/div/div/section/div/form/div[7]/div/div[2]')
-            blockchain_button.click()
-            polygon_button_location = '//span[normalize-space() = "Mumbai"]'
-            wait.until(expect_cond.presence_of_element_located((By.XPATH, polygon_button_location)))
-            polygon_button = web_driver.find_element(By.XPATH, polygon_button_location)
-            polygon_button.click()
+    ext_link = web_driver.find_element_by_xpath('//*[@id="external_link"]')
+    ext_link.send_keys(item.collection_ext_link)
+    time.sleep(0.5)
 
-        create = web_driver.find_element_by_xpath(
-            '//*[@id="__next"]/div[1]/main/div/div/section/div[2]/form/div/div[1]/span/button')
-        web_driver.execute_script("arguments[0].click();", create)
-        time.sleep(1)
+    desc = web_driver.find_element_by_xpath('//*[@id="description"]')
+    desc.send_keys(item.item_description)
+    time.sleep(0.5)
 
-        wait_css_selector("i[aria-label='Close']")
-        cross = web_driver.find_element_by_css_selector("i[aria-label='Close']")
-        cross.click()
-        time.sleep(1)
+    # Select Polygon blockchain if applicable
+    if is_polygon.get():
+        blockchain_button = \
+            web_driver.find_element(By.XPATH,
+                                    '//*[@id="__next"]/div[1]/main/div/div/section/div/form/div[7]/div/div[2]')
+        blockchain_button.click()
+        polygon_button_location = '//span[normalize-space() = "Mumbai"]'
+        wait_xpath(wait, polygon_button_location)
+        polygon_button = web_driver.find_element(By.XPATH, polygon_button_location)
+        polygon_button.click()
 
-        main_page = web_driver.current_window_handle
-        wait_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/div/span[2]/a')
-        sell = web_driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/div/span[2]/a')
-        sell.click()
+    create = web_driver.find_element_by_xpath(
+        '//*[@id="__next"]/div[1]/main/div/div/section/div[2]/form/div/div[1]/span/button')
+    web_driver.execute_script("arguments[0].click();", create)
+    time.sleep(1)
 
-        wait_css_selector("input[placeholder='Amount']")
-        amount = web_driver.find_element_by_css_selector("input[placeholder='Amount']")
-        amount.send_keys(str(item_price))
+    wait_css_selector(wait, "i[aria-label='Close']")
+    cross = web_driver.find_element_by_css_selector("i[aria-label='Close']")
+    cross.click()
+    time.sleep(1)
 
-        wait_css_selector("button[type='submit']")
-        listing = web_driver.find_element_by_css_selector("button[type='submit']")
-        listing.click()
-        # wait until the window is available.
-        while len(web_driver.window_handles) != 2:
-            print('.', end='')
-            time.sleep(.5)
+    main_page = web_driver.current_window_handle
+    wait_xpath(wait, '//*[@id="__next"]/div[1]/main/div/div/div[1]/div/span[2]/a')
+    sell = web_driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div[1]/div/span[2]/a')
+    sell.click()
 
-        login_page = web_driver.window_handles[1]
+    wait_css_selector(wait, "input[placeholder='Amount']")
+    amount = web_driver.find_element_by_css_selector("input[placeholder='Amount']")
+    amount.send_keys(item.item_price)
 
-        for handle in web_driver.window_handles:
-            if handle != main_page:
-                login_page = handle
-        # change the control to signin page
-        web_driver.switch_to.window(login_page)
-        wait_css_selector("button[data-testid='request-signature__sign']")
-        sign = web_driver.find_element_by_css_selector("button[data-testid='request-signature__sign']")
-        sign.click()
-        time.sleep(1)
+    wait_css_selector(wait, "button[type='submit']")
+    listing = web_driver.find_element_by_css_selector("button[type='submit']")
+    listing.click()
 
-        # change control to main page
-        web_driver.switch_to.window(main_page)
-        time.sleep(1)
+    # wait until the window is available.
+    while len(web_driver.window_handles) != 2:
+        print('.', end='')
+        time.sleep(.5)
 
-        start_num = start_num + 1
-        print('NFT creation completed!')
+    login_page = web_driver.window_handles[1]
+
+    for handle in web_driver.window_handles:
+        if handle != main_page:
+            login_page = handle
+
+    # change the control to sign-in page
+    web_driver.switch_to.window(login_page)
+    wait_css_selector(wait, "button[data-testid='request-signature__sign']")
+    sign = web_driver.find_element_by_css_selector("button[data-testid='request-signature__sign']")
+    sign.click()
+    time.sleep(1)
+
+    # change control to main page
+    web_driver.switch_to.window(main_page)
+    time.sleep(1)
+
+    item.increment_current_item()
+    print('NFT creation completed!')
 
 
 def init_chrome_options(in_project_path):
@@ -180,7 +210,7 @@ def init_chrome_options(in_project_path):
     return w_driver
 
 
-def wait_css_selector(css):
+def wait_css_selector(wait, css):
     """Helper Function wait for css """
     try:
         wait.until(expect_cond.presence_of_element_located((By.CSS_SELECTOR, css)))
@@ -188,7 +218,7 @@ def wait_css_selector(css):
         print(f"Timed out waiting for element: [{css:s}]")
 
 
-def wait_xpath(xpath):
+def wait_xpath(wait, xpath):
     """Helper Function wait for xpath"""
     try:
         wait.until(expect_cond.presence_of_element_located((By.XPATH, xpath)))
@@ -218,8 +248,10 @@ def source_folder_location():
     side effect: modifies path_to_nfts
     :return: None
     """
+    default_path = folder_location.input_field.get()
     global path_to_nfts
-    path_to_nfts = filedialog.askdirectory()
+    path_to_nfts = filedialog.askdirectory(initialdir=default_path) if os.path.isdir(default_path) \
+        else filedialog.askdirectory()
     if path_to_nfts:
         folder_location.insert_text(path_to_nfts)
 
@@ -235,5 +267,5 @@ upload_folder_input_button = tkinter.Button(gui, width=20, text="NFTs Source Fol
 upload_folder_input_button.grid(row=21, column=0, sticky='w')
 
 if __name__ == '__main__':
-    # Start Tk
+    """ Start Tk """
     gui.mainloop()
